884283a41031d4fe71a4b4dc4dfae3968f81a6f8
mana71 <mana71@mail.ru>
2016-12-22 00:17:24 +0300

    add timeout

--- java-example/src/test/java/ru/stqa/lesson2/task3/LoginToAdmin.java
+++ java-example/src/test/java/ru/stqa/lesson2/task3/LoginToAdmin.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 public class LoginToAdmin {
 
 
@@ -18,6 +20,7 @@ public class LoginToAdmin {
     @Before
     public void start() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
