60cdbdb98a8638081f9053900e6a589ffcf5b9ba
mana71 <mana71@mail.ru>
2016-12-22 00:08:24 +0300

    lesson2task3

---
+++ java-example/src/test/java/ru/stqa/lesson2/task3/LoginToAdmin.java
@@ -0,0 +1,37 @@
+package ru.stqa.lesson2.task3;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginToAdmin {
+
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void loginToAdmin (){
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void quit(){
+        driver.quit();
+        driver = null;
+    }
+}
