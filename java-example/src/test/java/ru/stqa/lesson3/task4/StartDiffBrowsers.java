a9f2c001b0ec8729c4c1c1859b69f9ebbbd3656d
Maria Benavides Martines <mana71@mail.ru>
2016-12-22 18:32:58 +0300

    Revert "next changes"
    This reverts commit 5ee34bf830e243c72433bd921e74ca1a0785ea35.

--- java-example/src/test/java/ru/stqa/lesson3/task4/StartDiffBrowsers.java
+++ java-example/src/test/java/ru/stqa/lesson3/task4/StartDiffBrowsers.java
@@ -4,9 +4,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -16,19 +13,9 @@ public class StartDiffBrowsers {
 
     private WebDriver driverChr, driverFf, driverIe;
     private WebDriverWait waitChr, waitFf, waitIe;
-
+  
 
     @Before
-    public void start(){
-        driverChr = new ChromeDriver();
-        waitChr = new WebDriverWait(driverChr, 10);
-        driverFf = new FirefoxDriver();
-        waitFf = new WebDriverWait(driverFf, 10);
-        driverIe = new InternetExplorerDriver();
-        waitIe = new WebDriverWait(driverIe, 10);
-
-
-    }
 
 
 
