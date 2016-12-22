6c359530ed11c92b5513bb1c10591de43cc34c75
Maria Benavides Martines <mana71@mail.ru>
2016-12-22 18:21:08 +0300

    StartDiffBrowsers
    task4

---
+++ java-example/src/test/java/ru/stqa/lesson3/task4/StartDiffBrowsers.java
@@ -0,0 +1,27 @@
+package ru.stqa.lesson3.task4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Created by Maria on 12/22/2016.
+ */
+public class StartDiffBrowsers {
+
+    private WebDriver driverChr, driverFf, driverIe;
+    private WebDriverWait waitChr, waitFf, waitIe;
+  
+
+    @Before
+
+
+
+    @Test
+
+
+
+    @After
+}
