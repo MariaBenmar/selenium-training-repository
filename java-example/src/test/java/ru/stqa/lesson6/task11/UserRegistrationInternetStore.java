f1fb86e9d193a1322be6a88975034c8b4cadec44
mana71 <mana71@mail.ru>
2017-01-19 01:18:06 +0300

    lesson6 task11 finished

--- java-example/src/test/java/ru/stqa/lesson6/task11/UserRegistrationInternetStore.java
+++ java-example/src/test/java/ru/stqa/lesson6/task11/UserRegistrationInternetStore.java
@@ -13,28 +13,27 @@ public class UserRegistrationInternetStore extends BaseTest{
     String locatorTaxId, locatorCompany, locatorFirstName, locatorLastName, locatorAddrLine1, locatorAddrLine2,
             locatorPostcode, locatorCity, locatorCountryCode, locatorZoneCode, locatorEmail, locatorPhone,
             locatorNewsletter, locatorPassword, locatorConfirmPassword, locatorCreateAccountButton,
-            locatorAcceptCookiesButton, locatorLogoutLink, locatorNewCustomerLink,locatorLoginButton;
+             locatorLogoutLink, locatorNewCustomerLink,locatorLoginButton;
     String textTaxId, textCompany, textFirstName, textLastName, textAddrLine1, textAddrLine2,
             textPostcode, textCity, textCountryCode, textZoneCode, textEmail, textPhone,
-            textNewsletter, textPassword, textConfirmPassword;
-    WebElement taxId, company, firstName, lastName, addrLine1, addrLine2, postcode, city, countryCode, zoneCode,
-            email, phone, newsletter, password, confirmPassword, createAccountButton,
-            logoutLink, newCustomerLink, loginButton;
+             textPassword, textConfirmPassword;
+    WebElement createAccountButton, logoutLink, newCustomerLink, loginButton;
 
-    public void fillText(String text, WebElement locator){
+    public void fillText(String text, String locatorSearch){
+        WebElement locator = driver.findElement(By.cssSelector(locatorSearch));
         locator.clear();
         locator.sendKeys(text);
     }
 
-    public void fillSelectForm(String code, WebElement locator){
-        Select menuItem = new Select(locator);
+    public void fillSelectForm(String code, String locatorSearch){
+        Select menuItem = new Select(driver.findElement(By.cssSelector(locatorSearch)));
         menuItem.selectByValue(code);
     }
 
     @Test
     public void userRegistrationInternetStore(){
         driver.navigate().to("http://localhost/litecart/en/");
-    }
+
 
     locatorTaxId = "input[name='tax_id']";
     locatorCompany = "input[name='company']";
@@ -51,12 +50,10 @@ public class UserRegistrationInternetStore extends BaseTest{
     locatorNewsletter = "input[name='newsletter']";
     locatorPassword = "input[name='password']";
     locatorConfirmPassword = "input[name='confirmed_password']";
+
     locatorCreateAccountButton = "button[name='create_account']";
-    locatorAcceptCookiesButton = "input[name='accept_cookies']";
-
-    locatorLogoutLink = "//div[@id = 'box-account']//div[@class = 'content']//a[text()='Logout']";
-
-    locatorNewCustomerLink ="/*//*[@id='box-account-login']//a[text()='New customers click here']";
+    locatorLogoutLink = "#box-account > div > ul > li:nth-child(4) > a";
+    locatorNewCustomerLink ="#box-account-login > div > form > table > tbody > tr:nth-child(5) > td > a";
     locatorLoginButton = "button[name = 'login']";
 
     // new account data
@@ -66,59 +63,42 @@ public class UserRegistrationInternetStore extends BaseTest{
     textLastName = "BenMart";
     textAddrLine1 = "AddrLine1";
     textAddrLine2 = "AddrLine2";
-    textZoneCode = "190185";
+    textPostcode = "190185";
     textCity = "Spb";
-    textCountryCode = "UA";
-    textEmail = "mariabm@domain.com";
+    textCountryCode = "RU";
+    textEmail = "mariabm+1@domain.com";
     textPhone = "+71234567890";
     textPassword = "Qwerty_1234";
     textConfirmPassword = "Qwerty_1234";
 
     newCustomerLink = driver.findElement(By.cssSelector(locatorNewCustomerLink));
-        newCustomerLink.click();
-    taxId = driver.findElement(By.cssSelector(locatorTaxId));
-    fillText (textTaxId, taxId);
-    company = driver.findElement(By.cssSelector(locatorCompany));
-    fillText (textCompany, company);
-    firstName = driver.findElement(By.cssSelector(locatorFirstName));
-    fillText (textFirstName, firstName);
-    lastName = driver.findElement(By.cssSelector(locatorLastName));
-    fillText (textLastName, lastName);
-    addrLine1 = driver.findElement(By.cssSelector(locatorAddrLine1));
-    fillText (textAddrLine1, addrLine1);
-    addrLine2 = driver.findElement(By.cssSelector(locatorAddrLine2));
-    fillText (textAddrLine2, addrLine2);
-    zoneCode = driver.findElement(By.cssSelector(locatorZoneCode));
-    fillText (textZoneCode, zoneCode);
-    city = driver.findElement(By.cssSelector(locatorCity));
-    fillText (textCity, city);
-    countryCode = driver.findElement(By.cssSelector(locatorCountryCode));
-    fillSelectForm (textCountryCode, countryCode);
-//        Select country = new Select(Country);
-//        country.selectByValue(code);
-    email = driver.findElement(By.cssSelector(locatorEmail));
-    fillText (textEmail, email);
-    phone = driver.findElement(By.cssSelector(locatorPhone));
-    fillText (textPhone, phone);
-    password = driver.findElement(By.cssSelector(locatorPassword));
-    fillText (textPassword, password);
-    confirmPassword = driver.findElement(By.cssSelector(locatorConfirmPassword));
-    fillText (textConfirmPassword, confirmPassword);
+    newCustomerLink.click();
+    fillText (textTaxId, locatorTaxId);
+    fillText (textCompany, locatorCompany);
+    fillText (textFirstName, locatorFirstName);
+    fillText (textLastName, locatorLastName);
+    fillText (textAddrLine1, locatorAddrLine1);
+    fillText (textAddrLine2, locatorAddrLine2);
+    fillText (textPostcode, locatorPostcode);
+    fillText (textCity, locatorCity);
+    fillSelectForm (textCountryCode, locatorCountryCode);
+    fillText (textEmail, locatorEmail);
+    fillText (textPhone, locatorPhone);
+    fillText (textPassword, locatorPassword);
+    fillText (textConfirmPassword, locatorConfirmPassword);
+
     createAccountButton = driver.findElement(By.cssSelector(locatorCreateAccountButton));
         createAccountButton.click();
 
     logoutLink = driver.findElement(By.cssSelector(locatorLogoutLink));
         logoutLink.click();
-    email = driver.findElement(By.cssSelector(locatorEmail));
-    fillText (textEmail, email);
-    password = driver.findElement(By.cssSelector(locatorPassword));
-    fillText (textPassword, password);
+    fillText (textEmail, locatorEmail);
+    fillText (textPassword, locatorPassword);
 
     loginButton = driver.findElement(By.cssSelector(locatorLoginButton));
         loginButton.click();
 
     logoutLink = driver.findElement(By.cssSelector(locatorLogoutLink));
         logoutLink.click();
-}
-
+    }
 }
